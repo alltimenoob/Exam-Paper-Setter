920c63b4e9d713fb696df633e759fe49fb663c96
mihir <mihirchauhan756@gmail.com>
2022-06-30 17:03:37 +0530

    ODT in Response

--- server/odt/views.py
+++ server/odt/views.py
@@ -18,5 +18,9 @@ def getODT(request):
 
     os.system(
         "soffice --infilter=\"writer_pdf_import\" --convert-to odf:\"writer8\" "+file_name)
-    response = HttpResponse("Done")
+
+    file = default_storage.open(file_name)
+
+    response = HttpResponse(file, content_type='application/odt')
+    response['Content-Disposition'] = 'attachment; filename=' + file_name
     return response
